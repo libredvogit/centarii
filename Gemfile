51b8724679cc078df445d21668f20953abdec762
dvonik <dvogit@tuta.io>
2021-12-16 06:07:58 -0500

    updated readme and stuff

---
+++ Gemfile
@@ -0,0 +1,2 @@
+source "https://rubygems.org"
+gem "discordrb", git: "https://github.com/shardlab/discordrb"
--- main.rb
+++ main.rb
@@ -2,15 +2,15 @@
 
 require "discordrb"
 
+# Read from configuration
+# - make sure Config is of Hash
 eval File.read "./config.rb"
 
-@bot = Discordrb::Commands::CommandBot.new token: Config[0], client_id: Config[1]
+bot = Discordrb::Commands::CommandBot.new token: Config["token"], client_id: Config["clientID"], intents: [:server_messages]
 
-# bot.register_application_command :example "Commands", ser
-
-@bot.message with_text: "/ping" do |event|
+bot.message with_text: ".ping" do |event|
     m = event.respond "pong ..."
     m.edit "pong #{Time.now - event.timestamp}s"
 end
 
-@bot.run
+bot.run
